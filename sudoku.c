5d520ee84cbc05c08bc86f897e59535186f797e2
ridik18.03 <ridik18.03@mail.ru>
2013-05-14 11:24:55 +0300

    changed structure to simply int [][]

--- sudoku.c
+++ sudoku.c
@@ -2,11 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX 9
-struct cell {
-    int value;
-    int possible[MAX];
-    int nofoundinother[MAX];
-} cell[MAX][MAX];
+int cell[MAX][MAX];
 void enterSudoku(FILE * file, char *filename)
 {
     int i, j;
@@ -15,7 +11,7 @@ void enterSudoku(FILE * file, char *filename)
     file = fopen(filename, "r");
     for (i = 0; i < MAX; i++) {
         for (j = 0; j < MAX; j++) {
-            fscanf(file, "%d", &cell[i][j].value);
+            fscanf(file, "%d", &cell[i][j]);
         }
     }
     fclose(file);
@@ -28,11 +24,11 @@ void displaySudoku(FILE * file, char *filename)
     for (i = 0; i < MAX; i++) {
         for (j = 0; j < MAX; j++) {
             if (j == 2 || j == 5 || j == 8) {
-                printf("%3d ", cell[i][j].value);
+                printf("%3d ", cell[i][j]);
             }
 
             else {
-                printf("%3d", cell[i][j].value);
+                printf("%3d", cell[i][j]);
             }
         }
         puts("");
@@ -42,7 +38,7 @@ void displaySudoku(FILE * file, char *filename)
     }
     for (i = 0; i < MAX; i++) {
         for (j = 0; j < MAX; j++) {
-            fprintf(file, "%d ", cell[i][j].value);
+            fprintf(file, "%d ", cell[i][j]);
         }
         fprintf(file, "\n");
     }
@@ -51,34 +47,34 @@ void displaySudoku(FILE * file, char *filename)
 
 int chck_sdk()
 {
-    int fail = 0;               // вначале полагаем, что в таблице все четко
+    int fail = 0;
     int i;
     for (i = 0; i < 9 && !fail; ++i) {
-        int p[9] = { 0, };      // 0 числа нет, 1 число есть
+        int p[9] = { 0, };
         int j;
         for (j = 0; j < 9 && !fail; ++j) {
-            if (cell[i][j].value == 0) {
+            if (cell[i][j] == 0) {
                 continue;
             }
-            if (p[cell[i][j].value - 1] == 0)
-                p[cell[i][j].value - 1] = 1;
+            if (p[cell[i][j] - 1] == 0)
+                p[cell[i][j] - 1] = 1;
 
             else
-                fail = 1;       // два одинаковых числа в строке
+                fail = 1;
         }
     }
     int j;
     for (j = 0; j < 9 && !fail; ++j) {
         int p[9] = { 0, };
         for (i = 0; i < 9 && !fail; ++i) {
-            if (cell[i][j].value == 0) {
+            if (cell[i][j] == 0) {
                 continue;
             }
-            if (p[cell[i][j].value - 1] == 0)
-                p[cell[i][j].value - 1] = 1;
+            if (p[cell[i][j] - 1] == 0)
+                p[cell[i][j] - 1] = 1;
 
             else
-                fail = 1;       // два одинаковых числа в столбце
+                fail = 1;
         }
     }
     for (i = 0; i < 9 && !fail; i += 3) {
@@ -89,14 +85,13 @@ int chck_sdk()
             for (ik = i; ik < i + 3 && !fail; ++ik) {
                 int jk;
                 for (jk = j; jk < j + 3 && !fail; ++jk) {
-                    if (cell[ik][jk].value == 0) {
+                    if (cell[ik][jk] == 0) {
                         continue;
                     }
-                    if (p[cell[ik][jk].value - 1] == 0)
-                        p[cell[ik][jk].value - 1] = 1;
-
+                    if (p[cell[ik][jk] - 1] == 0)
+                        p[cell[ik][jk] - 1] = 1;
                     else
-                        fail = 1;       // два одинаковых числа в квадрате - секторе
+                        fail = 1;
                 }
             }
         }
@@ -104,7 +99,6 @@ int chck_sdk()
     if (!fail) {
         return 0;
     }
-
     else {
         return 1;
     }
@@ -114,7 +108,7 @@ int solveSudoku(FILE * file, int i, int j)
 {
     int n = 1;
     while (1) {
-        if (cell[i][j].value != 0) {
+        if (cell[i][j] != 0) {
             if ((i + 1) >= MAX && (j + 1) >= MAX) {
                 return 0;
             }
@@ -127,20 +121,19 @@ int solveSudoku(FILE * file, int i, int j)
                 j++;
             }
         }
-
         else {
             break;
         }
     }
     while (1) {
         while (1) {
-            cell[i][j].value = n;
+            cell[i][j] = n;
             n++;
             if (chck_sdk() == 0) {
                 break;
             }
             if (n >= 10) {
-                cell[i][j].value = 0;
+                cell[i][j] = 0;
                 return 1;
             }
         }
@@ -161,7 +154,6 @@ int main()
         puts("Bad sudoku");
         return 0;
     }
-
     else {
         displaySudoku(file, "output.txt");
     }
